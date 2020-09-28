ef104370c411a0246fc3871eb5f565244bc703b1
Ricardo Pellegrini <ricardo.pellegrini92@gmail.com>
2020-09-28 17:33:14 -0300

    adding new tests

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,46 @@
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+// import User from '../infra/typeorm/entities/User';
+import { inject, injectable } from 'tsyringe';
+
+
+interface IRequest {
+  password: string;
+  token: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ password, token }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exist');
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+
+    user.password = await this.hashProvider.generateHash(password);
+
+    await this.usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
